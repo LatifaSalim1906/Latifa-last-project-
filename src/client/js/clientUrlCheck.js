b25753d29e86c7ba8039057901aff585a237ad68
LatifaSalim1906 <latifa_salim@hotmail.com>
2024-12-31 14:19:55 +0400

    Delete src directory

--- src/client/js/clientUrlCheck.js
+++
@@ -1,7 +0,0 @@
-function checkUrl(userInput) {
-    var txt_res = userInput.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-    if( txt_res == null)
-        return false;
-    else
-        return true;
-}
